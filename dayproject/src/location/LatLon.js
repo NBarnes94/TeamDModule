f34293ec24d8f6bfe84ef768f365c821b0540790
Nick Barnes <Nicholas.Barnes4194@.com>
2021-06-08 16:52:33 -0500

    fist draft of build with dynamic functionality

--- dayproject/src/location/LatLon.js
+++ dayproject/src/location/LatLon.js
@@ -1,21 +1,48 @@
-import React from 'react';
-import {Input,FormGroup, Label, Form, Button} from 'reactstrap';
+import React, { useState } from 'react';
+import { Input, FormGroup, Label, Form, Button } from 'reactstrap';
+import Tm from '../TM/TM';
 
 
 
-const LatLon = (props) =>{
-    return(
-        <Form>
-            <FormGroup>
-                <Label>Longitude</Label>
-                <Input />
-            </FormGroup>
-            <FormGroup>
-                <Label>Latitude</Label>
-                <Input />
-            </FormGroup>
-            <Button>What is happening here?</Button>
-        </Form>
+const LatLon = (props) => {
+    console.log(props);
+    const [latitude, setLatitude] = useState('');
+    const [longitude, setLongitude] = useState('')
+    const [tmresults, setTmResults] = useState([]);
+
+
+    const fetchTMResults = () => {
+        let url = `https://app.ticketmaster.com/discovery/v2/events?apikey=eDb4BQQtXjPJwP2mdVa6eLtiQBnqU0Os&latlong=${latitude},${longitude}&locale=*`;
+
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setTmResults(data._embedded.events))
+            .catch(err => console.log(err));
+            // console.log(data);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        fetchTMResults();
+    };
+
+    return (
+        <div className='searchFunction'>
+            <Form onSubmit={(e) => handleSubmit(e)}>
+                <FormGroup>
+                    <Label>Latitude</Label>
+                    <Input type='text' name="latitude" onChange={(e) => setLatitude(e.target.value)} required />
+                </FormGroup>
+                <FormGroup>
+                    <Label>Longitude</Label>
+                    <Input type='text' name='longitude' onChange={(e) => setLongitude(e.target.value)} required />
+                </FormGroup>
+                <Button className='submit'>What is happening here?</Button>
+            </Form>
+            {
+                tmresults.length >0? <Tm results={tmresults}/> : null
+            }
+        </div>
     )
 }
 
