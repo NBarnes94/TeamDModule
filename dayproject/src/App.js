c30577871138a06932c0446a01a4f28dfa3e67b8
Thomas Glover <thomasglover@Thomass-MacBook-Pro.local>
2021-06-08 19:43:42 -0400

    added Nasa component

--- dayproject/src/App.js
+++ dayproject/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import LatLon from './location/LatLon'
+import FetchNasa from './Nasa/Nasa';
 
 function App() {
   return (
     <div className="location">
       <LatLon />
+      <FetchNasa />
     </div>
   );
 }
---
+++ dayproject/src/Nasa/Nasa.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+
+
+
+
+
+const FetchNasa = (props) => {
+    const lat = 1.5;
+    const lon = 100;
+    const [imageUrl, setImageUrl] = useState('');
+    const key='bbEkFc14hCjvXhEWL0IhGL24szQLATccadTiij0k';
+    const url = `https://api.nasa.gov/planetary/earth/assets?lon=${lon}&lat=${lat}&api_key=${key}`;
+    
+
+    fetch(url)
+    .then(response => response.json())
+    .then(json => setImageUrl(json.url))
+    .catch(err => console.log(err))
+    
+    
+    return(
+            <div>
+                <img src={imageUrl} alt="Location"></img>
+            </div>
+            
+        
+    )
+    }
+
+
+export default FetchNasa;
